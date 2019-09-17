0b82874ea2b3d473f64f5d522ac8edba907a33c3
Adam Frank White <adamfrankwhite@gmail.com>
2019-09-17 10:40:13 +0100

    added share functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import SaveImg from './SaveImg'
 
 function Card(props) {
     const cardStyle = {
         backgroundImage: `url(${props.src})`,
-        backgroundPosition: "center"
+        backgroundPosition: "center",
+        backgroundSize: "cover"
     }
     
     return (
-        <div className="card" style={cardStyle}>
+        <div className={props.multi === 1 ? "single-card" : "card"} style={cardStyle}>
             {/* <img src={props.src}/> */}
             <h3 className="img-caption">{props.caption}</h3>
+            <SaveImg imgData={props.imgData} />
+            <ul className="social-media">
+                <a href={`http://www.facebook.com/sharer.php?u=${props.imgData[1]}`} target="_blank"><li><i className="fa fa-facebook" aria-hidden="true"></i></li></a>
+                <a href={`https://twitter.com/share?url=${props.imgData[1]}&amp;text=${props.caption};hashtags=NASA`} target="_blank"><li><i className="fa fa-twitter" aria-hidden="true"></i></li></a>
+                <a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());"><li><i className="fa fa-instagram" aria-hidden="true"></i></li></a>
+                <a href={`http://reddit.com/submit?url=${props.imgData[0]}`} target="_blank"><li><i className="fa fa-reddit" aria-hidden="true"></i></li></a>
+            </ul>
         </div>
     )
 
